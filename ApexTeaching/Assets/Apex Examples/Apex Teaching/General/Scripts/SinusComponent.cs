948edfb913f50f97fa7007340136e47f2e0ea47b
Rami Ahmed Bock <Rami@ApexGameTools.com>
2016-03-03 16:17:03 +0100

    Sinus component only moves if the unit is not moving (and it is on a unit)

--- ApexTeaching/Assets/Apex Examples/Apex Teaching/General/Scripts/SinusComponent.cs
+++ ApexTeaching/Assets/Apex Examples/Apex Teaching/General/Scripts/SinusComponent.cs
@@ -1,27 +1,33 @@
-﻿using UnityEngine;
-
-namespace Assets.Resources.Scripts.Components
+﻿namespace Apex.AI.Teaching
 {
+    using UnityEngine;
+
     public class SinusComponent : MonoBehaviour
     {
         public Vector3 start;
         public Vector3 magnitude;
         public Vector3 direction;
 
+        private UnitBase _unit;
+
         public void OnEnable()
         {
+            _unit = this.transform.GetComponentInParent<UnitBase>();
         }
 
         private void Update()
         {
-            var x = Mathf.Sin(direction.x * Time.time + start.x) * magnitude.x;
-            var y = Mathf.Sin(direction.y * Time.time + start.y) * magnitude.y;
-            var z = Mathf.Sin(direction.z * Time.time + start.z) * magnitude.z;
+            if (_unit != null && !_unit.isMoving)
+            {
+                this.transform.localPosition = Vector3.zero;
+                return;
+            }
 
-            var v = new Vector3(x, y, z);
-
-            //this.transform.position += transform.TransformDirection(v);
-            this.transform.localPosition += v;
+            var time = Time.time;
+            var x = Mathf.Sin(direction.x * time + start.x) * magnitude.x;
+            var y = Mathf.Sin(direction.y * time + start.y) * magnitude.y;
+            var z = Mathf.Sin(direction.z * time + start.z) * magnitude.z;
+            this.transform.localPosition += new Vector3(x, y, z);
         }
     }
 }
