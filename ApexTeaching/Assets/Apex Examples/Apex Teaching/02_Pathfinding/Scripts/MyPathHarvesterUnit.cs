0b0fd79c1f618a724ea83eb9536d69f335c85b43
Rami Ahmed Bock <ramiahmedbock@gmail.com>
2016-03-11 10:17:55 +0100

    Prepared for lecture with "empty" classes and own 'MyNest'

---
+++ ApexTeaching/Assets/Apex Examples/Apex Teaching/02_Pathfinding/Scripts/MyPathHarvesterUnit.cs
@@ -0,0 +1,27 @@
+﻿namespace Apex.AI.Teaching
+{
+    using UnityEngine;
+
+    public class MyPathHarvesterUnit : HarvesterUnit
+    {
+        public override bool isMoving
+        {
+            get { return base.isMoving; }
+        }
+
+        public override void MoveTo(Vector3 destination)
+        {
+            base.MoveTo(destination);
+        }
+
+        public override void RandomWander()
+        {
+            base.RandomWander();
+        }
+
+        public override void StopMoving()
+        {
+            base.StopMoving();
+        }
+    }
+}
