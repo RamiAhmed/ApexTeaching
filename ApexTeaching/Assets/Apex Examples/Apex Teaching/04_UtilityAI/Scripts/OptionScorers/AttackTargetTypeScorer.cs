936a4bb212964a9cada2d2b2b1c91c1c5ee3b95b
Rami Ahmed Bock <Rami@ApexGameTools.com>
2016-04-19 12:41:51 +0200

    Added an attack target type scorer for scoring nest or unit type

---
+++ ApexTeaching/Assets/Apex Examples/Apex Teaching/04_UtilityAI/Scripts/OptionScorers/AttackTargetTypeScorer.cs
@@ -0,0 +1,38 @@
+﻿namespace Apex.AI.Teaching
+{
+    using Apex.Serialization;
+
+    public sealed class AttackTargetTypeScorer : OptionScorerBase<ICanDie>
+    {
+        [ApexSerialization]
+        public float score;
+
+        [ApexSerialization]
+        public bool onlyNest;
+
+        [ApexSerialization]
+        public UnitType unitType;
+
+        public override float Score(IAIContext context, ICanDie option)
+        {
+            if (this.onlyNest)
+            {
+                if (option is NestStructure)
+                {
+                    return this.score;
+                }
+            }
+
+            var unit = option.gameObject.GetComponent<UnitBase>();
+            if (unit != null)
+            {
+                if (unit.type == this.unitType)
+                {
+                    return this.score;
+                }
+            }
+
+            return 0f;
+        }
+    }
+}
